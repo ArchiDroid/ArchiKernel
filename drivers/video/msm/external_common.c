af5840ff25c178e0a1f94330b6905ac986c4364c
Nagamalleswararao Ganji <nganji@codeaurora.org>
2012-07-03 20:06:41 -0700

    msm: display: use the fb reserved fields same as kernel 3.0
    Reserved fields of fb structure used for various use cases
    in qcom fb driver. Reducing the reserved fields caused lot
    of side effects and it will change the reserved fields same
    as kernel 3.0.
    
    Change-Id: I7bf82a9b5667cb5c84b54e428bbfcb6869157bd7
    Signed-off-by: Nagamalleswararao Ganji <nganji@codeaurora.org>

--- drivers/video/msm/external_common.c
+++ drivers/video/msm/external_common.c
@@ -1978,8 +1978,8 @@ bool hdmi_common_get_video_format_from_drv_data(struct msm_fb_data_type *mfd)
 	struct fb_var_screeninfo *var = &mfd->fbi->var;
 	bool changed = TRUE;
 
-	if (var->reserved[2]) {
-		format = var->reserved[2]-1;
+	if (var->reserved[3]) {
+		format = var->reserved[3]-1;
 		DEV_DBG("reserved format is %d\n", format);
 	} else {
 		DEV_DBG("detecting resolution from %dx%d use var->reserved[3]"
