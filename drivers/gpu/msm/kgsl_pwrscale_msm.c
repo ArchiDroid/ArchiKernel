6780d28cc2e4f659990a10fc152befee48a944c9
Suman Tatiraju <sumant@codeaurora.org>
2012-08-23 14:21:33 -0700

    msm: kgsl: Don't report io_busy for msm policy
    Turn io_fraction to 100 so that the kernel
    wait_io_interruptible call is not required for io_busy
    reporting while using msm policy. Msm policy uses its
    own schema to capture busy events.
    
    Change-Id: Iddb63552305974ce4a12446117f27d07b7201387
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>
    Signed-off-by: Rajeev Kulkarni <krajeev@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrscale_msm.c
+++ drivers/gpu/msm/kgsl_pwrscale_msm.c
@@ -114,6 +114,28 @@ static void msm_sleep(struct kgsl_device *device,
 	/* do we need to reset any parameters here? */
 }
 
+static void msm_remove_io_fraction(struct kgsl_device *device)
+{
+	int i;
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+
+	for (i = 0; i < pwr->num_pwrlevels; i++)
+		pwr->pwrlevels[i].io_fraction = 100;
+
+}
+
+static void msm_restore_io_fraction(struct kgsl_device *device)
+{
+	int i;
+	struct kgsl_device_platform_data *pdata =
+				kgsl_device_get_drvdata(device);
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+
+	for (i = 0; i < pdata->num_levels; i++)
+		pwr->pwrlevels[i].io_fraction =
+			pdata->pwrlevel[i].io_fraction;
+}
+
 static int msm_init(struct kgsl_device *device,
 		     struct kgsl_pwrscale *pwrscale)
 {
@@ -164,6 +186,7 @@ static int msm_init(struct kgsl_device *device,
 		} else {
 			device->pwrscale.gpu_busy = 1;
 		}
+		msm_remove_io_fraction(device);
 		return 0;
 	}
 
@@ -188,6 +211,7 @@ static void msm_close(struct kgsl_device *device,
 	msm_dcvs_freq_sink_unregister(&priv->freq_sink);
 	kfree(pwrscale->priv);
 	pwrscale->priv = NULL;
+	msm_restore_io_fraction(device);
 }
 
 struct kgsl_pwrscale_policy kgsl_pwrscale_policy_msm = {
