8e14af961e5d0e2cdbfb54b6388263753824075c
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-09-11 15:59:11 -0700

    ion: change ion buffer flags
    This syncs up our ion buffer flags in ion.h with upstream (using
    ION_FLAG_CACHED instead of CACHED and UNCACHED).
    
    Change-Id: I2e07a3acba230a86817182677fa2394fa846fd7e
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -63,7 +63,6 @@ enum ion_heap_type {
              maintenance when the buffer is
              mapped for dma */
 
-
 /**
  * These are the only ids that should be used for Ion heap ids.
  * The ids listed are the order in which allocation will be attempted
@@ -139,11 +138,11 @@ enum cp_mem_usage {
 #define CACHED          1
 #define UNCACHED        0
 
-#define ION_CACHE_SHIFT 0
+#define ION_SET_CACHED(__cache)		(__cache | ION_FLAG_CACHED)
+#define ION_SET_UNCACHED(__cache)	(__cache & ~ION_FLAG_CACHED)
 
-#define ION_SET_CACHE(__cache)  ((__cache) << ION_CACHE_SHIFT)
+#define ION_IS_CACHED(__flags)	((__flags) & ION_FLAG_CACHED)
 
-#define ION_IS_CACHED(__flags)	((__flags) & (1 << ION_CACHE_SHIFT))
 
 /*
  * This flag allows clients when mapping into the IOMMU to specify to
