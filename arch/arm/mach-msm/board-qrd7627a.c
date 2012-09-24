eb9acd8c5f6f8357414c344973d7285a881625d8
Taniya Das <tdas@codeaurora.org>
2012-09-24 17:59:14 +0530

    msm: platsmp-8625: cleanup macro names
    All the macros defined for 8625 secondary core indicates dual
    core. Cleanup macros to be more generic to fit for multi core.
    
    Change-Id: Ia21b2fe6e4a980ae1d60729ceb4085d1a72a16d3
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- arch/arm/mach-msm/board-qrd7627a.c
+++ arch/arm/mach-msm/board-qrd7627a.c
@@ -825,8 +825,8 @@ static void __init msm7627a_reserve(void)
 
 static void __init msm8625_reserve(void)
 {
-	memblock_remove(MSM8625_SECONDARY_PHYS, SZ_8);
 	memblock_remove(MSM8625_NON_CACHE_MEM, SZ_2K);
+	memblock_remove(MSM8625_CPU_PHYS, SZ_8);
 	msm7627a_reserve();
 }
 
