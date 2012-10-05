f73c1bbb9a3674b7da220b0450df619edc0a5099
Mayank Agarwal <magarw@codeaurora.org>
2012-10-05 16:29:26 +0530

    msm: camera: Fix camcorder hang for 120 fps with aptina 8MP
    Increasing the vfe_clk_rate to max in board file for streaming
    HFR 120 fps.
    
    CR Fixed:406997
    Signed-off-by: Mayank Agarwal <magarw@codeaurora.org>
    (cherry picked from commit 038bcce1eff978f4e506becf02f46b7bb9d2b0a3)
    
    Change-Id: I49b01a1d8d3d63d7a9980622d2d2cc2592223621
    Signed-off-by: Gopal Goberu <ggober@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60-camera.c
+++ arch/arm/mach-msm/board-msm8x60-camera.c
@@ -364,7 +364,7 @@ static struct msm_camera_device_platform_data msm_camera_csi_device_data[] = {
 		.is_vpe    = 1,
 		.cam_bus_scale_table = &cam_bus_client_pdata,
 		.ioclk = {
-			.vfe_clk_rate =	228570000,
+			.vfe_clk_rate =	266667000,
 		},
 	},
 	{
