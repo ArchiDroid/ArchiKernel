freedreno-zz/kernel-msm

msm_fb: display: Increase MDP IOMMU buffer mapping size
Double the MDP IOMMU buffer mapping size to allow overfetching
by hardware for boundary conditions and avoid system crashes
due to page faults.

CRs-Fixed: 378108
CRs-Fixed: 364478
Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
Change-Id: Ib19af6b06ab06455acaffcca33830e0487bb0836
(cherry picked from commit 1ed0dae869f6384d23322b78691852d70c573174)

Conflicts:

	drivers/video/msm/mdp4_overlay.c

Signed-off-by: Dhivya Subramanian <dthiru@codeaurora.org>

drivers/video/msm/mdp4_overlay.c
@@ -108,6 +108,8 @@ int mdp4_overlay_iommu_map_buf(int mem_id,
 	struct ion_handle **srcp_ihdl)
 {
 	struct mdp4_iommu_pipe_info *iom_pipe_info;
+	unsigned long size = 0;
+	int ret;
 
 	if (!display_iclient)
 		return -EINVAL;
@@ -117,15 +119,23 @@ int mdp4_overlay_iommu_map_buf(int mem_id,
 		pr_err("ion_import_fd() failed\n");
 		return PTR_ERR(*srcp_ihdl);
 	}
-	pr_debug("%s(): ion_hdl %p, ion_buf %p\n", __func__, *srcp_ihdl,
-		ion_share(display_iclient, *srcp_ihdl));
+
+	ret = ion_handle_get_size(display_iclient, *srcp_ihdl, &size);
+	if (ret)
+		pr_err("ion_handle_get_size failed with ret %d\n", ret);
+	else
+		size *= 2;
+
+	pr_debug("%s(): ion_hdl %p, size 0x%lx\n", __func__,
+		*srcp_ihdl, size);
 	pr_debug("mixer %u, pipe %u, plane %u\n", pipe->mixer_num,
 		pipe->pipe_ndx, plane);
 	if (ion_map_iommu(display_iclient, *srcp_ihdl,
-		DISPLAY_READ_DOMAIN, GEN_POOL, SZ_4K, 0, start,
+		DISPLAY_READ_DOMAIN, GEN_POOL, SZ_4K, size, start,
 		len, 0, ION_IOMMU_UNMAP_DELAYED)) {
 		ion_free(display_iclient, *srcp_ihdl);
-		pr_err("ion_map_iommu() failed\n");
+		pr_err("%s(): ion_map_iommu() failed\n",
+			__func__);
 		return -EINVAL;
 	}
 

ArchiDroid/ArchiKernel

drivers/video/msm/mdp4_overlay.c
@@ -215,6 +215,8 @@ int mdp4_overlay_iommu_map_buf(int mem_id,
 	struct ion_handle **srcp_ihdl)
 {
 	struct mdp4_iommu_pipe_info *iom;
+	unsigned long size = 0;
+	int ret;
 
 	if (!display_iclient)
 		return -EINVAL;
@@ -224,15 +226,23 @@ int mdp4_overlay_iommu_map_buf(int mem_id,
 		pr_err("ion_import_dma_buf() failed\n");
 		return PTR_ERR(*srcp_ihdl);
 	}
-	pr_debug("%s(): ion_hdl %p, ion_buf %p\n", __func__, *srcp_ihdl,
-		ion_share(display_iclient, *srcp_ihdl));
+
+	ret = ion_handle_get_size(display_iclient, *srcp_ihdl, &size);
+	if (ret)
+		pr_err("ion_handle_get_size failed with ret %d\n", ret);
+	else
+		size *= 2;
+
+	pr_debug("%s(): ion_hdl %p, size 0x%lx\n", __func__,
+		*srcp_ihdl, size);
 	pr_debug("mixer %u, pipe %u, plane %u\n", pipe->mixer_num,
 		pipe->pipe_ndx, plane);
 	if (ion_map_iommu(display_iclient, *srcp_ihdl,
-		DISPLAY_DOMAIN, GEN_POOL, SZ_4K, 0, start,
+		DISPLAY_DOMAIN, GEN_POOL, SZ_4K, size, start,
 		len, 0, ION_IOMMU_UNMAP_DELAYED)) {
 		ion_free(display_iclient, *srcp_ihdl);
-		pr_err("ion_map_iommu() failed\n");
+		pr_err("%s(): ion_map_iommu() failed\n",
+			__func__);
 		return -EINVAL;
 	}
 