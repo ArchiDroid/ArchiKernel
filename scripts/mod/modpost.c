8dfd99cc2e6dcfb7f29216c4729e2125eb62cc16
Stephen Smalley <sds@tycho.nsa.gov>
2012-11-05 08:15:34 -0500

    Add security hooks to binder and implement the hooks for  SELinux.
    Add security hooks to the binder and implement the hooks for SELinux.
    The security hooks enable security modules such as SELinux to implement
    controls over binder IPC.  The security hooks include support for
    controlling what process can become the binder context manager
    (binder_set_context_mgr), controlling the ability of a process
    to invoke a binder transaction/IPC to another process (binder_transaction),
    controlling the ability a process to transfer a binder reference to
    another process (binder_transfer_binder), and controlling the ability
    of a process to transfer an open file to another process (binder_transfer_file).
    
    This support is used by SE Android, http://selinuxproject.org/page/SEAndroid.
    
    Change-Id: I9a64a87825df2e60b9c51400377af4a9cd1c4049
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1851,7 +1851,7 @@ static void add_header(struct buffer *b, struct module *mod)
 	buf_printf(b, "MODULE_INFO(vermagic, VERMAGIC_STRING);\n");
 	buf_printf(b, "\n");
 	buf_printf(b, "struct module __this_module\n");
-	buf_printf(b, "__attribute__((section(\".gnu.linkonce.this_module\"))) = {\n");
+	buf_printf(b, "__attribute__((externally_visible,section(\".gnu.linkonce.this_module\"))) = {\n");
 	buf_printf(b, " .name = KBUILD_MODNAME,\n");
 	if (mod->has_init)
 		buf_printf(b, " .init = init_module,\n");
