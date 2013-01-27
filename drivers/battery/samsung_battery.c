78682c28d010c3e04691221e6802ed62c1a5b07c
andip71 <andreasp@gmx.de>
2013-01-27 14:26:35 +0100

    samsung_battery: Charge level interface
    - defaults are stock currents
    - now separate interfaces for ac and usb

--- drivers/battery/samsung_battery.c
+++ drivers/battery/samsung_battery.c
@@ -49,6 +49,12 @@
 #include <linux/fake_shut_down.h>
 #endif
 
+#include "linux/charge_level.h"
+
+int ac_level = AC_CHARGE_LEVEL_DEFAULT;    // Set AC default charge level
+int usb_level  = USB_CHARGE_LEVEL_DEFAULT; // Set USB default charge level
+
+
 static char *supply_list[] = {
 	"battery",
 };
@@ -1721,30 +1727,25 @@ charge_ok:
 		info->recharge_phase = false;
 		break;
 	case POWER_SUPPLY_TYPE_MAINS:
+		printk("Boeffla-Kernel: POWER_SUPPLY_TYPE_MAINS, using charge rate %d mA\n", ac_level);
 		if (!info->pdata->suspend_chging)
 			wake_lock(&info->charge_wake_lock);
-#if defined(CONFIG_MACH_KONA)
-		if(mhl_connected==true)
-			battery_charge_control(info,info->pdata->chg_curr_mhl,
-				info->pdata->chg_curr_mhl);
-		else
-#endif
-		battery_charge_control(info, info->pdata->chg_curr_ta,
-						info->pdata->in_curr_limit);
+		battery_charge_control(info, ac_level, ac_level);
 		break;
 	case POWER_SUPPLY_TYPE_USB:
+		printk("Boeffla-Kernel: POWER_SUPPLY_TYPE_USB, using charge rate %d mA\n", usb_level);
 		if (!info->pdata->suspend_chging)
 			wake_lock(&info->charge_wake_lock);
-		battery_charge_control(info, info->pdata->chg_curr_usb,
-						info->pdata->chg_curr_usb);
+		battery_charge_control(info, usb_level, usb_level);
 		break;
 	case POWER_SUPPLY_TYPE_USB_CDP:
+		printk("Boeffla-Kernel: POWER_SUPPLY_TYPE_USB_CDP, using charge rate %d mA\n", ac_level);
 		if (!info->pdata->suspend_chging)
 			wake_lock(&info->charge_wake_lock);
-		battery_charge_control(info, info->pdata->chg_curr_cdp,
-						info->pdata->chg_curr_cdp);
+		battery_charge_control(info, ac_level, ac_level);
 		break;
 	case POWER_SUPPLY_TYPE_DOCK:
+		printk("Boeffla-Kernel: POWER_SUPPLY_TYPE_DOCK\n");
 		if (!info->pdata->suspend_chging)
 			wake_lock(&info->charge_wake_lock);
 		/* default dock prop is AC */
@@ -1752,41 +1753,36 @@ charge_ok:
 		muic_cb_typ = max77693_muic_get_charging_type();
 		switch (muic_cb_typ) {
 		case CABLE_TYPE_AUDIODOCK_MUIC:
+			printk("Boeffla-Kernel: CABLE_TYPE_AUDIODOCK_MUIC, using charge rate %d mA\n", ac_level);
 			pr_info("%s: audio dock, %d\n",
 					__func__, DOCK_TYPE_AUDIO_CURR);
-			battery_charge_control(info,
-						DOCK_TYPE_AUDIO_CURR,
-						DOCK_TYPE_AUDIO_CURR);
+			battery_charge_control(info, ac_level, ac_level);
 			break;
 		case CABLE_TYPE_SMARTDOCK_TA_MUIC:
+			printk("Boeffla-Kernel: CABLE_TYPE_SMARTDOCK_TA_MUIC (with host), using charge rate %d mA\n", ac_level);
 			if (info->cable_sub_type == ONLINE_SUB_TYPE_SMART_OTG) {
 				pr_info("%s: smart dock ta & host, %d\n",
 					__func__, DOCK_TYPE_SMART_OTG_CURR);
-				battery_charge_control(info,
-						DOCK_TYPE_SMART_OTG_CURR,
-						DOCK_TYPE_SMART_OTG_CURR);
+				battery_charge_control(info, ac_level, ac_level);
 			} else {
+				printk("Boeffla-Kernel: CABLE_TYPE_SMARTDOCK_TA_MUIC (no host), using charge rate %d mA\n", ac_level);
 				pr_info("%s: smart dock ta & no host, %d\n",
 					__func__, DOCK_TYPE_SMART_NOTG_CURR);
-				battery_charge_control(info,
-						DOCK_TYPE_SMART_NOTG_CURR,
-						DOCK_TYPE_SMART_NOTG_CURR);
+				battery_charge_control(info, ac_level, ac_level);
 			}
 			break;
 		case CABLE_TYPE_SMARTDOCK_USB_MUIC:
+			printk("Boeffla-Kernel: CABLE_TYPE_SMARTDOCK_USB_MUIC, using charge rate %d mA\n", ac_level);
 			pr_info("%s: smart dock usb(low), %d\n",
 					__func__, DOCK_TYPE_LOW_CURR);
 			info->online_prop = ONLINE_PROP_USB;
-			battery_charge_control(info,
-						DOCK_TYPE_LOW_CURR,
-						DOCK_TYPE_LOW_CURR);
+			battery_charge_control(info, ac_level, ac_level);
 			break;
 		default:
+			printk("Boeffla-Kernel: General dock, using charge rate %d mA\n", ac_level);
 			pr_info("%s: general dock, %d\n",
 					__func__, info->pdata->chg_curr_dock);
-		battery_charge_control(info,
-			info->pdata->chg_curr_dock,
-			info->pdata->chg_curr_dock);
+			battery_charge_control(info, ac_level, ac_level);
 			break;
 		}
 		break;
