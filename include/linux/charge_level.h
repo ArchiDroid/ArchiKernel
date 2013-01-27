76a85d8e38c95fa6f4b923a790a13e91ea9ea2f4
andip71 <andreasp@gmx.de>
2013-01-27 14:26:35 +0100

    samsung_battery: Charge level interface
    - defaults are stock currents
    - now separate interfaces for ac and usb
    
    Conflicts:
    	arch/arm/mach-exynos/Kconfig

---
+++ include/linux/charge_level.h
@@ -0,0 +1,27 @@
+/*
+ * Author: andip71, 27.01.2013
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+extern int ac_level;
+
+#define AC_CHARGE_LEVEL_DEFAULT 1000
+
+#define AC_CHARGE_LEVEL_MIN 100
+#define AC_CHARGE_LEVEL_MAX 1600
+
+extern int usb_level;
+
+#define USB_CHARGE_LEVEL_DEFAULT 475
+
+#define USB_CHARGE_LEVEL_MIN 0
+#define USB_CHARGE_LEVEL_MAX 1600
