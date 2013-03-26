c426afcdca048739d0f4039622fd0daf3609463c
Kuogee Hsieh <khsieh@codeaurora.org>
2013-03-25 17:05:44 -0700

    msm_fb: display: allow backlight update after first update
    bl_updated is set to non zero at pan_display if unset_bl_level
    is not zero. And unset_bl_level is not updated unless bl_updated
    is zero. There is a situation that bl_updated stay as zero after
    first pan display due to unset_bl_level is still zero which
    prevents back light from being changed until next pan display
    even though unset_bl_level changed. This patch break up this loop
    and allow back light being changed right after first pan display.
    
    CRs-fixed: 471983
    Change-Id: I75504206853517672f968e8ea8fc6782a282e326
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1139,12 +1139,11 @@ void msm_fb_set_backlight(struct msm_fb_data_type *mfd, __u32 bkl_lvl)
 {
 	struct msm_fb_panel_data *pdata;
 	__u32 temp = bkl_lvl;
-	if (!mfd->panel_power_on || !bl_updated) {
-		unset_bl_level = bkl_lvl;
+
+	unset_bl_level = bkl_lvl;
+
+	if (!mfd->panel_power_on || !bl_updated)
 		return;
-	} else {
-		unset_bl_level = 0;
-	}
 
 	pdata = (struct msm_fb_panel_data *)mfd->pdev->dev.platform_data;
 
@@ -2350,7 +2349,7 @@ static int msm_fb_pan_display_sub(struct fb_var_screeninfo *var,
 	msm_fb_signal_timeline(mfd);
 	up(&msm_fb_pan_sem);
 
-	if (unset_bl_level && !bl_updated)
+	if (!bl_updated)
 		schedule_delayed_work(&mfd->backlight_worker,
 				backlight_duration);
 
