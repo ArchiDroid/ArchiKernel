e9b32affdcd4d7db89bd6d4d622285db28ace104
RonGokhale <cips173@gmail.com>
2013-05-23 08:12:26 +0530

    Revert "msm: kgsl: Hold the pm_qos vote at all times when  ACTIVE"
    This reverts commit 66ad4c0ed14ecbe0fbbd24b5bdcd5ca984ea7386.
    
    Change-Id: I1c70a8d5a76544abe89484576917f2dd8142342a

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1248,8 +1248,9 @@ void kgsl_pwrctrl_wake(struct kgsl_device *device)
 		/* Re-enable HW access */
 		mod_timer(&device->idle_timer,
 				jiffies + device->pwrctrl.interval_timeout);
-		pm_qos_update_request(&device->pm_qos_req_dma,
-					GPU_SWFI_LATENCY);
+		if (device->pwrctrl.restore_slumber == false)
+			pm_qos_update_request(&device->pm_qos_req_dma,
+						GPU_SWFI_LATENCY);
 	case KGSL_STATE_ACTIVE:
 		kgsl_pwrctrl_request_state(device, KGSL_STATE_NONE);
 		break;
