154cc1ed890ea0a8a6c8adf6617e53fadd3e75d4
Carter Cooper <ccooper@codeaurora.org>
2013-05-28 17:04:26 -0600

    msm: kgsl: Move timestamps inside the context structure
    Store the timestamps inside the context rather than in a
    list stored in the ringbuffer.  This allows for easier
    maintanability as well as keeping all context data
    centralized.
    
    Change-Id: I0467d07be6c8bb9f062a81a40629c0288be7e868
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/adreno_drawctxt.c
+++ drivers/gpu/msm/adreno_drawctxt.c
@@ -147,7 +147,6 @@ int adreno_drawctxt_create(struct kgsl_device *device,
 {
 	struct adreno_context *drawctxt;
 	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
-	struct adreno_ringbuffer *rb = &adreno_dev->ringbuffer;
 	int ret;
 
 	drawctxt = kzalloc(sizeof(struct adreno_context), GFP_KERNEL);
@@ -160,7 +159,7 @@ int adreno_drawctxt_create(struct kgsl_device *device,
 	drawctxt->pagetable = pagetable;
 	drawctxt->bin_base_offset = 0;
 	drawctxt->id = context->id;
-	rb->timestamp[context->id] = 0;
+	drawctxt->timestamp = 0;
 
 	if (flags & KGSL_CONTEXT_PREAMBLE)
 		drawctxt->flags |= CTXT_FLAGS_PREAMBLE;
