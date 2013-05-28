e48cb2e4fd8a189081877390e6b53c116bed1476
Shubhraprakash Das <sadas@codeaurora.org>
2013-05-28 17:08:10 -0600

    msm: kgsl: Sync memory with CFF from places where it was  missing
    Before submitting any indirect buffer to GPU via the ringbuffer,
    the indirect buffer memory should be synced with CFF so that the
    CFF capture will be complete. Add the syncing of memory with CFF
    in places where this was missing
    
    Change-Id: I18f506dd1ab7bdfb1a68181016e6f661a36ed5a2
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -31,6 +31,7 @@
 #include "adreno.h"
 #include "kgsl_trace.h"
 #include "z180.h"
+#include "kgsl_cffdump.h"
 
 
 static struct kgsl_iommu_register_list kgsl_iommuv1_reg[KGSL_IOMMU_REG_MAX] = {
@@ -1103,6 +1104,11 @@ static int kgsl_iommu_start(struct kgsl_mmu *mmu)
 		}
 	}
 
+        /* For complete CFF */
+	kgsl_cffdump_setmem(mmu->setstate_memory.gpuaddr +
+				KGSL_IOMMU_SETSTATE_NOP_OFFSET,
+				cp_nop_packet(1), sizeof(unsigned int));
+
 	kgsl_iommu_disable_clk_on_ts(mmu, 0, false);
 	mmu->flags |= KGSL_FLAGS_STARTED;
 
@@ -1334,6 +1340,8 @@ static void kgsl_iommu_default_setstate(struct kgsl_mmu *mmu,
 
 	/* Release GPU-CPU sync Lock here */
 	msm_iommu_unlock();
+
+        
 
 	/* Disable smmu clock */
 	kgsl_iommu_disable_clk_on_ts(mmu, 0, false);
