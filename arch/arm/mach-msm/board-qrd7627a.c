fc90a0423de5da0c6fbb6b4373993a83d52fc7ee
Sravan Kumar Ambapuram <asravan@codeaurora.org>
2013-06-05 00:59:12 +0530

    msm: board7627a: Disabling ZSL feature on low RAM devices
    ZSL feature is enabled only for devices with more than 512MB DDR
    or 512MB DDR and CMA enabled.
    
    Change-Id: I2c5f56090d03a09f273903eca7af0d410a0a84c2
    Signed-off-by: Sravan Kumar Ambapuram <asravan@codeaurora.org>

--- arch/arm/mach-msm/board-qrd7627a.c
+++ arch/arm/mach-msm/board-qrd7627a.c
@@ -684,7 +684,9 @@ static void fix_sizes(void)
 #ifdef CONFIG_ION_MSM
 	msm_ion_audio_size = MSM_PMEM_AUDIO_SIZE;
 #ifdef CONFIG_CMA
-	msm_ion_camera_size = CAMERA_ZSL_SIZE;
+        if (get_ddr_size() > SZ_256M)
+                pmem_adsp_size = CAMERA_ZSL_SIZE;
+	msm_ion_camera_size = pmem_adsp_size;
 	msm_ion_camera_size_carving = 0;
 #else
 	msm_ion_camera_size = pmem_adsp_size;
