5888e83488cf471b611804eea2f5266380924bc0
Katta Santhisindhu <kattas@codeaurora.org>
2013-09-16 14:45:39 +0530

    msm-camera: Fix various small issues in Actuator driver
    Bound check and validate userspace parameters direction,
    number of steps and direction sign. Also fix possible
    memory leak in certain error cases.
    
    Change-Id: I05a3f63583845851df36b122f2079d345657cb99
    CRs-Fixed: 511349
    
    Signed-off-by: Katta Santhisindhu <kattas@codeaurora.org>

--- include/media/msm_camera.h
+++ include/media/msm_camera.h
@@ -225,7 +225,7 @@ struct msm_mctl_post_proc_cmd {
 #define MAX_ACTUATOR_INIT_SET 12
 #define MAX_ACTUATOR_TYPE_SIZE 32
 #define MAX_ACTUATOR_REG_TBL_SIZE 8
-
+#define MAX_ACTUATOR_AF_TOTAL_STEPS 1024
 
 #define MSM_MAX_CAMERA_CONFIGS 2
 
@@ -233,6 +233,8 @@ struct msm_mctl_post_proc_cmd {
 #define PP_RAW_SNAP ((0x01)<<1)
 #define PP_PREV  ((0x01)<<2)
 #define PP_THUMB ((0x01)<<3)
+#define MSM_ACTUATOR_MOVE_SIGNED_FAR -1
+#define MSM_ACTUATOR_MOVE_SIGNED_NEAR  1
 #define PP_MASK		(PP_SNAP|PP_RAW_SNAP|PP_PREV|PP_THUMB)
 
 #define MSM_CAM_CTRL_CMD_DONE  0
