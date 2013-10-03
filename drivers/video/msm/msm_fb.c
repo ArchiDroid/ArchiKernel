c04650fb6c940131a2da2ca5dd45d5552f233398
Krishna Chaitanya Parimi <cparimi@codeaurora.org>
2013-10-03 18:46:53 +0530

    msm: fb: Null check mfd data
    Add appropriate null check for mfd data structure before
    accessing it's members. This prevents null pointer accesses
    in the driver.
    
    Change-Id: I5aae937f839e4f6ed8d5c5a4b54ab45f0ff07332
    Signed-off-by: Krishna Chaitanya Parimi <cparimi@codeaurora.org>
    Signed-off-by: Neti, Ravi Kumar <ravineti@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -4058,7 +4058,7 @@ static int msmfb_get_metadata(struct msm_fb_data_type *mfd,
 static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 			unsigned long arg)
 {
-	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
+	struct msm_fb_data_type *mfd;
 	void __user *argp = (void __user *)arg;
 	struct fb_cursor cursor;
 	struct fb_cmap cmap;
@@ -4076,6 +4076,10 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
         struct mdp_buf_sync buf_sync;
 	int ret = 0;
 	msm_fb_pan_idle(mfd);
+
+	if (!info || !info->par)
+		return -EINVAL;
+	mfd = (struct msm_fb_data_type *)info->par;
 
 	switch (cmd) {
 #ifdef CONFIG_FB_MSM_OVERLAY
