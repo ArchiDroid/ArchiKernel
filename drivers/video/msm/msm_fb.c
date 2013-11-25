ae2dd82abe0539b6b0a291b340f2836cd3379ac8
RonGokhale <cips173@gmail.com>
2013-11-25 15:40:22 +0530

    fix msm_fb build after latest merge
    Change-Id: I39e70dc12cc86f26dead5b677a3a1d53ff760933

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -3813,8 +3813,7 @@ static void msmfb_set_color_conv(struct mdp_csc *p)
 
 static int msmfb_notify_update(struct fb_info *info, void __user *argp)
 {
-	int ret;
-	unsigned int notify;
+	unsigned int ret = 0, notify = 0;
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
 
 	ret = copy_from_user(&notify, argp, sizeof(unsigned int));
@@ -4076,11 +4075,13 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 	struct msmfb_metadata mdp_metadata;
         struct mdp_buf_sync buf_sync;
 	int ret = 0;
-	msm_fb_pan_idle(mfd);
 
-	if (!info || !info->par)
-		return -EINVAL;
-	mfd = (struct msm_fb_data_type *)info->par;
+	if (!info || !(info->par))
+                 return -EINVAL;
+ 
+         mfd = (struct msm_fb_data_type *)info->par;
+
+        msm_fb_pan_idle(mfd);
 
 	switch (cmd) {
 #ifdef CONFIG_FB_MSM_OVERLAY
