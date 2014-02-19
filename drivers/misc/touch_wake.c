33e1e296bfdae4faf83a41931a95f79fd120f4a6
andip71 <andreasp@gmx.de>
2014-02-19 21:59:51 +0100

    Touch to wake: Add option to keep touch-to-wake active all the time when connected to charger (andip71)

--- drivers/misc/touch_wake.c
+++ drivers/misc/touch_wake.c
@@ -25,9 +25,11 @@
 #include <linux/delay.h>
 #include <linux/wakelock.h>
 #include <linux/input.h>
+#include <linux/battery/samsung_battery.h>
 
 extern void touchscreen_enable(void);
 extern void touchscreen_disable(void);
+extern unsigned int charge_info_cable_type;
 
 static bool touchwake_enabled = false;
 static bool touch_disabled = false;
@@ -35,6 +37,7 @@ static bool device_suspended = false;
 static bool timed_out = true;
 static bool prox_near = false;
 static unsigned int touchoff_delay = 45000;
+static bool charger_mode = false;
 
 static void touchwake_touchoff(struct work_struct * touchoff_work);
 static DECLARE_DELAYED_WORK(touchoff_work, touchwake_touchoff);
@@ -81,7 +84,15 @@ static void touchwake_early_suspend(struct early_suspend * h)
 	#endif
 
 	if (touchwake_enabled) {
-		if (likely(touchoff_delay > 0))	{
+		if ((charge_info_cable_type != POWER_SUPPLY_TYPE_BATTERY) && charger_mode)	{
+			if (timed_out && !prox_near) {
+				#ifdef DEBUG_PRINT
+				pr_info("[TOUCHWAKE] Charger plug mode - keep touch enabled indefinately\n");
+				#endif
+				wake_lock(&touchwake_wake_lock);
+			}
+		}
+		else if (likely(touchoff_delay > 0))	{
 			if (timed_out && !prox_near) {
 				#ifdef DEBUG_PRINT
 				pr_info("[TOUCHWAKE] Early suspend - enable touch delay\n");
@@ -172,6 +183,30 @@ static void press_powerkey(struct work_struct * presspower_work)
 	return;
 }
 
+static ssize_t touchwake_charger_mode_read(struct device * dev, struct device_attribute * attr, char * buf)
+{
+	return sprintf(buf, "%u\n", (charger_mode ? 1 : 0));
+}
+
+static ssize_t touchwake_charger_mode_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
+{
+	unsigned int ret = -EINVAL;
+	unsigned int val;
+
+	ret = sscanf(buf, "%d", &val);
+
+	if (val == 0)
+		charger_mode = false;
+	else if (val == 1)
+		charger_mode = true;
+
+	#ifdef DEBUG_PRINT
+	pr_info("[TOUCHWAKE] %s: Charger mode set to %d\n", __FUNCTION__, charger_mode);
+	#endif
+	
+	return size;
+}
+
 static ssize_t touchwake_status_read(struct device * dev, struct device_attribute * attr, char * buf)
 {
 	return sprintf(buf, "%u\n", (touchwake_enabled ? 1 : 0));
@@ -246,6 +281,7 @@ static ssize_t touchwake_debug(struct device * dev, struct device_attribute * at
 static DEVICE_ATTR(enabled, S_IRUGO | S_IWUGO, touchwake_status_read, touchwake_status_write);
 static DEVICE_ATTR(delay, S_IRUGO | S_IWUGO, touchwake_delay_read, touchwake_delay_write);
 static DEVICE_ATTR(version, S_IRUGO , touchwake_version, NULL);
+static DEVICE_ATTR(charger_mode, S_IRUGO | S_IWUGO, touchwake_charger_mode_read, touchwake_charger_mode_write);
 #ifdef DEBUG_PRINT
 static DEVICE_ATTR(debug, S_IRUGO , touchwake_debug, NULL);
 #endif
@@ -255,6 +291,7 @@ static struct attribute *touchwake_notification_attributes[] =
 	&dev_attr_enabled.attr,
 	&dev_attr_delay.attr,
 	&dev_attr_version.attr,
+	&dev_attr_charger_mode.attr,
 #ifdef DEBUG_PRINT
 	&dev_attr_debug.attr,
 #endif
