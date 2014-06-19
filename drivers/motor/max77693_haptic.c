7c8a83ba2bf488fbe09e702f010314e09e329553
MarcKe <herderkewitz@googlemail.com>
2014-06-19 16:33:21 +0200

    add haptic configuration (thx to Lord Boeffla)

--- drivers/motor/max77693_haptic.c
+++ drivers/motor/max77693_haptic.c
@@ -22,7 +22,8 @@
 #include <linux/mfd/max77693.h>
 #include <linux/mfd/max77693-private.h>
 
-//#define SEC_DEBUG_VIB
+static unsigned long pwm_val = 50; /* duty in percent */
+static int pwm_duty = 27787; /* duty value, 37050=100%, 27787=50%, 18525=0% */
 
 struct max77693_haptic_data {
 	struct max77693_dev *max77693;
@@ -157,8 +158,7 @@ static void haptic_work(struct work_struct *work)
 
 		max77693_haptic_i2c(hap_data, true);
 
-		pwm_config(hap_data->pwm, hap_data->pdata->duty,
-			   hap_data->pdata->period);
+		pwm_config(hap_data->pwm, pwm_duty, hap_data->pdata->period);
 		pwm_enable(hap_data->pwm);
 
 		if (hap_data->pdata->motor_en)
@@ -236,24 +236,17 @@ void vibtonz_pwm(int nForce)
 {
 	/* add to avoid the glitch issue */
 	static int prev_duty;
-	int pwm_period = 0, pwm_duty = 0;
+	int pwm_period = 0;
 
 	if (g_hap_data == NULL) {
-		printk(KERN_ERR "[VIB] the motor is not ready!!!");
+		pr_err("[VIB] %s: the motor is not ready!!!", __func__);
 		return ;
 	}
-
-	pwm_period = g_hap_data->pdata->period;
-	pwm_duty = pwm_period / 2 + ((pwm_period / 2 - 2) * nForce) / 127;
-
-	if (pwm_duty > g_hap_data->pdata->duty)
-		pwm_duty = g_hap_data->pdata->duty;
-	else if (pwm_period - pwm_duty > g_hap_data->pdata->duty)
-		pwm_duty = pwm_period - g_hap_data->pdata->duty;
 
 	/* add to avoid the glitch issue */
 	if (prev_duty != pwm_duty) {
 		prev_duty = pwm_duty;
+
 		pwm_config(g_hap_data->pwm, pwm_duty, pwm_period);
 	}
 #ifdef SEC_DEBUG_VIB
@@ -262,6 +255,68 @@ void vibtonz_pwm(int nForce)
 }
 EXPORT_SYMBOL(vibtonz_pwm);
 #endif
+
+static ssize_t pwm_val_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	int count;
+
+    pwm_val = ((pwm_duty - 18525) * 100) / 18525;
+
+	count = sprintf(buf, "%lu\n", pwm_val);
+	pr_debug("[VIB] pwm_val: %lu\n", pwm_val);
+
+	return count;
+}
+
+ssize_t pwm_val_store(struct device *dev,
+		struct device_attribute *attr,
+		const char *buf, size_t size)
+{
+	if (kstrtoul(buf, 0, &pwm_val))
+		pr_err("[VIB] %s: error on storing pwm_val\n", __func__); 
+
+    pr_info("[VIB] %s: pwm_val=%lu\n", __func__, pwm_val);
+
+    pwm_duty = (pwm_val * 18525) / 100 + 18525;
+
+    /* make sure new pwm duty is in range */
+    if(pwm_duty > 37050) 
+    {   
+        pwm_duty = 37050;
+    } 
+    else if (pwm_duty < 18525) 
+    {
+        pwm_duty = 18525;
+    }
+
+	pr_info("[VIB] %s: pwm_duty=%d\n", __func__, pwm_duty);
+
+	return size;
+}
+static DEVICE_ATTR(pwm_val, S_IRUGO | S_IWUGO,
+		pwm_val_show, pwm_val_store);
+
+static DEVICE_ATTR(pwm_value, S_IRUGO | S_IWUSR,
+		pwm_val_show, pwm_val_store);
+
+static int create_vibrator_sysfs(void)
+{
+	int ret;
+	struct kobject *vibrator_kobj;
+	vibrator_kobj = kobject_create_and_add("vibrator", NULL);
+	if (unlikely(!vibrator_kobj))
+		return -ENOMEM;
+
+	ret = sysfs_create_file(vibrator_kobj,
+			&dev_attr_pwm_val.attr);
+	if (unlikely(ret < 0)) {
+		pr_err("[VIB] sysfs_create_file failed: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
 
 static int max77693_haptic_probe(struct platform_device *pdev)
 {
@@ -325,6 +380,8 @@ static int max77693_haptic_probe(struct platform_device *pdev)
 	hap_data->tout_dev.get_time = haptic_get_time;
 	hap_data->tout_dev.enable = haptic_enable;
 
+    create_vibrator_sysfs();
+
 #ifdef CONFIG_ANDROID_TIMED_OUTPUT
 	error = timed_output_dev_register(&hap_data->tout_dev);
 	if (error < 0) {
@@ -333,7 +390,14 @@ static int max77693_haptic_probe(struct platform_device *pdev)
 		goto err_timed_output_register;
 	}
 #endif
-	printk(KERN_DEBUG "[VIB] timed_output device is registrated\n");
+	pr_err("[VIB] timed_output device is registrated\n");
+
+	/* User controllable pwm level */
+	error = device_create_file(hap_data->tout_dev.dev, &dev_attr_pwm_value);
+	if (error < 0) {
+		pr_err("[VIB] create sysfs fail: pwm_value\n");
+	}
+
 	pr_debug("[VIB] -- %s\n", __func__);
 
 	return error;
