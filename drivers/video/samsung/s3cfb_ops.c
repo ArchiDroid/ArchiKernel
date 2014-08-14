aba53ff5efbc26bcf60fd5d98eb8e6273aad8c1c
JustArchi <JustArchi@JustArchi.net>
2014-08-14 23:08:57 +0200

    WIP: Add ArchiKernel logo It works, but whole image is blue, I need to find out why

--- drivers/video/samsung/s3cfb_ops.c
+++ drivers/video/samsung/s3cfb_ops.c
@@ -53,6 +53,10 @@
 #include <plat/s5p-sysmmu.h>
 #endif
 
+#ifdef CONFIG_ARCHIKERNEL_SPLASH_LOGO
+#include "logo_archikernel.h"
+#endif
+
 #if defined(CONFIG_S6D7AA0_LSL080AL02)
 static unsigned int fb_busfreq_table[S3C_FB_MAX_WIN + 1] = {
 	100100,
@@ -145,6 +149,10 @@ int s3cfb_draw_logo(struct fb_info *fb)
 		memcpy(fb->screen_base, logo_virt_buf, fb->var.yres * fb->fix.line_length);
 		printk(KERN_INFO "Bootloader sent 'bootloaderfb' : %08X\n", bootloaderfb);
 	}
+
+#ifdef CONFIG_ARCHIKERNEL_SPLASH_LOGO
+	memcpy(fb->screen_base, LOGO_ARCHIKERNEL, fb->var.yres * fb->fix.line_length);
+#endif
 
 #endif /* #ifdef RGB_BOOTSCREEN */
 #endif
