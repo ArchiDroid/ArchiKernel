105ef2ca6b41715dd4e57da420d08787d5e72dd0
andip71 <andreasp@gmx.de>
2014-09-22 18:24:16 +0200

    charge_level: Improved sysfs input validation

--- arch/arm/mach-exynos/charge_level.c
+++ arch/arm/mach-exynos/charge_level.c
@@ -35,6 +35,9 @@ static ssize_t charge_level_ac_store(struct kobject *kobj, struct kobj_attribute
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+    if (ret != 1)
+        return -EINVAL;
+        
 	// check whether value is within the valid ranges and adjust accordingly
 	if (val > AC_CHARGE_LEVEL_MAX)
 		val = AC_CHARGE_LEVEL_MAX;
@@ -64,6 +67,9 @@ static ssize_t charge_level_usb_store(struct kobject *kobj, struct kobj_attribut
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+    if (ret != 1)
+        return -EINVAL;
+        
 	// check whether value is within the valid ranges and adjust accordingly
 	if (val > USB_CHARGE_LEVEL_MAX)
 		val = USB_CHARGE_LEVEL_MAX;
@@ -92,6 +98,9 @@ static ssize_t charge_level_wireless_store(struct kobject *kobj, struct kobj_att
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+    if (ret != 1)
+        return -EINVAL;
+        
 	// check whether value is within the valid ranges and adjust accordingly
 	if (val > WIRELESS_CHARGE_LEVEL_MAX)
 		val = WIRELESS_CHARGE_LEVEL_MAX;
@@ -121,6 +130,9 @@ static ssize_t ignore_unstable_power_store(struct kobject *kobj, struct kobj_att
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+    if (ret != 1)
+        return -EINVAL;
+        
 	// check whether value is valid
 	if ((val == 0) || (val == 1))
 		ignore_unstable_power = val;
@@ -144,6 +156,9 @@ static ssize_t ignore_safety_margin_store(struct kobject *kobj, struct kobj_attr
 	// read value from input buffer
 	ret = sscanf(buf, "%d", &val);
 
+    if (ret != 1)
+        return -EINVAL;
+        
 	// check whether value is valid
 	if ((val == 0) || (val == 1))
 		ignore_safety_margin = val;
