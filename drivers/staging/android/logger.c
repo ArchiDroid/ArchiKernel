6d9bfef04f7b89d02f2b10768e4e44261271d60f
JustArchi <JustArchi@JustArchi.net>
2015-02-26 15:58:27 +0100

    AK: Implement logcat interface
    Conflicts:
    	archikernel/Kconfig
    	drivers/staging/android/Makefile

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -26,7 +26,9 @@
 #include <linux/slab.h>
 #include <linux/time.h>
 #include "logger.h"
+#ifdef CONFIG_ARCHIKERNEL_LOGCAT_INTERFACE
 #include "logger_interface.h"
+#endif
 
 #include <asm/ioctls.h>
 
@@ -397,11 +399,13 @@ static void do_write_log(struct logger_log *log, const void *buf, size_t count)
 {
 	size_t len;
 
+#ifdef CONFIG_ARCHIKERNEL_LOGCAT_INTERFACE
 	// if logger mode is disabled, terminate instantly
 	if (logger_mode == 0)
 	{
 			return;
-	} 
+	}
+#endif
         
 	len = min(count, log->size - log->w_off);
 	memcpy(log->buffer + log->w_off, buf, len);
@@ -426,11 +430,13 @@ static ssize_t do_write_log_from_user(struct logger_log *log,
 {
 	size_t len;
 
+#ifdef CONFIG_ARCHIKERNEL_LOGCAT_INTERFACE
 	// if logger mode is disabled, terminate instantly
 	if (logger_mode == 0)
 	{
 			return 0;
-	} 
+	}
+#endif
 
 	len = min(count, log->size - log->w_off);
 	if (len && copy_from_user(log->buffer + log->w_off, buf, len))
