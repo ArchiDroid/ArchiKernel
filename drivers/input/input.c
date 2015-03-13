bc1f65361d2899aa758d9fca5673895a8e1653ca
JustArchi <JustArchi@JustArchi.net>
2015-03-13 18:09:15 +0100

    TouchWake: Derp fix

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -250,7 +250,7 @@ static void input_handle_event(struct input_dev *dev,
 	case EV_KEY:
 		if (is_event_supported(code, dev->keybit, KEY_MAX) &&
 		    !!test_bit(code, dev->key) != value) {
-#ifdef CONFIG_TOUCH_WAKE
+#ifdef CONFIG_ARCHIKERNEL_TOUCH_WAKE
       if (code == KEY_POWER && !device_is_suspended()) {
         if (value == 1) {
           powerkey_pressed();
