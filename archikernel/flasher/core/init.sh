76cc11931606968a582453bb04d8361407a964ba
JustArchi <JustArchi@JustArchi.net>
2015-09-16 13:13:24 +0200

    Flasher: misc

--- archikernel/flasher/core/init.sh
+++ archikernel/flasher/core/init.sh
@@ -308,6 +308,9 @@ else
 	dd if="$AK/newboot.img" of="$KERNEL"
 fi
 
+echo "INFO: Cleaning old Synapse config"
+rm -rf /data/data/com.af.synapse
+
 echo "SUCCESS: Everything finished successfully!"
 date
 touch "$AK/_OK"
